a83ae3759a1a83b33237cd842bf3ca178908ca76
Jasmine502 <79311091+Jasmine502@users.noreply.github.com>
2023-05-07 13:56:39 +0100

    Updated Poe token, renamed variables

--- Unamon Generator/Discord Bot/bot.py
+++ Unamon Generator/Discord Bot/bot.py
@@ -14,27 +14,27 @@ def generate_words():
     response = requests.get("https://random-word-api.herokuapp.com/word?number=2")
     return response.json()
 
-# Function to get Fakemon name and description from Poe API
-def get_fakemon_info(client, words):
+# Function to get Unamon name and description from Poe API
+def get_unamon_info(client, words):
     # Construct message for Poe API
-    message = f"Come up with a unique Fakemon, along with its typing(s) and a detailed design, that combines the concepts of '{words[0]}' and '{words[1]}'. Please make sure that the name of the Fakemon MUST be the very first word of the text"
+    message = f"Come up with a unique Unamon (a Fakemon of my universe), along with its typing(s) and a detailed design, that combines the concepts of '{words[0]}' and '{words[1]}'. Please make sure that the name of the Unamon MUST be the very first word of the text"
     
     # Send message to Poe API
     response = ""
     for chunk in client.send_message("chinchilla", message):
         response += chunk["text_new"]
     
-    # Remove any non-alphabetic characters from Fakemon name
-    fakemon_name = re.sub(r"[^a-zA-Z]", "", response.split()[0])
-    fakemon_desc = response.strip()
+    # Remove any non-alphabetic characters from Unamon name
+    unamon_name = re.sub(r"[^a-zA-Z]", "", response.split()[0])
+    unamon_desc = response.strip()
 
-    return fakemon_name, fakemon_desc
+    return unamon_name, unamon_desc
 
-def generate_stats(client, fakemon_name):
-    # Prompt for Fakemon stats
-    prompt = f"Come up with the appropriate following stats/info for {fakemon_name}:\n" \
+def generate_stats(client, unamon_name):
+    # Prompt for Unamon stats
+    prompt = f"Come up with the appropriate following stats/info for {unamon_name}:\n" \
              "Signature Move (with a short description), it's PP, Attack DMG, Accuracy %, the move's type, the effectiveness percentage, and the level it's learned\n" \
-             "The Category of the Fakemon\n" \
+             "The Category of the Unamon\n" \
              "It's height and weight in imperial units\n" \
              "It's stats (HP, ATK, DEF, SpA, SpD and Speed).\n" \
              "Evolves from/into (if any)\n" \
@@ -49,19 +49,19 @@ def generate_stats(client, fakemon_name):
     return response
 
 
-def get_fakemon_by_name(user_id, name):
-    with open("caught_fakemon.txt", "r") as f:
-        caught_fakemon = json.load(f)
-    if str(user_id) not in caught_fakemon:
+def get_unamon_by_name(user_id, name):
+    with open("caught_unamon.txt", "r") as f:
+        caught_unamon = json.load(f)
+    if str(user_id) not in caught_unamon:
         return None
-    for fakemon in caught_fakemon[str(user_id)]:
-        if fakemon["name"] == name:
-            return fakemon
+    for unamon in caught_unamon[str(user_id)]:
+        if unamon["name"] == name:
+            return unamon
     return None
 
 
-def get_training_fakemon_by_user(user_id):
-    with open("fakemon_training.txt", "r") as f:
+def get_training_unamon_by_user(user_id):
+    with open("unamon_training.txt", "r") as f:
         lines = f.readlines()
         for line in lines:
             parts = line.strip().split(",")
@@ -70,17 +70,17 @@ def get_training_fakemon_by_user(user_id):
     return None
 
 
-def update_training_fakemon(user_id, fakemon_name, level, current_xp):
+def update_training_unamon(user_id, unamon_name, level, current_xp):
     new_lines = []
-    with open("fakemon_training.txt", "r") as f:
+    with open("unamon_training.txt", "r") as f:
         for line in f:
             line_parts = line.strip().split(",")
-            if line_parts[0] == str(user_id) and line_parts[1] == fakemon_name:
-                new_lines.append(f"{user_id},{fakemon_name},{level},{current_xp}")
+            if line_parts[0] == str(user_id) and line_parts[1] == unamon_name:
+                new_lines.append(f"{user_id},{unamon_name},{level},{current_xp}")
             else:
                 new_lines.append(line.strip())
 
-    with open("fakemon_training.txt", "w") as f:
+    with open("unamon_training.txt", "w") as f:
         f.write("\n".join(new_lines))
 
 @client.event
@@ -129,7 +129,7 @@ async def gen(ctx, *words):
             if str(reaction.emoji) == '👍':
                 break
             elif str(reaction.emoji) == '❌':
-                embed = discord.Embed(description=f"Cancelled Fakemon generation.", color=discord.Color.red())
+                embed = discord.Embed(description=f"Cancelled Unamon generation.", color=discord.Color.red())
                 embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
                 await ctx.send(embed=embed)
                 return
@@ -143,28 +143,28 @@ async def gen(ctx, *words):
 
     # If custom words provided, validate the input
     elif len(words) != 2:
-        embed = discord.Embed(description=f"Please provide either 2 words, or no words to generate a Fakemon.", color=discord.Color.red())
+        embed = discord.Embed(description=f"Please provide either 2 words, or no words to generate a Unamon.", color=discord.Color.red())
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         await ctx.send(embed=embed)
         return
 
- # Generate Fakemon description with Poe
-    embed = discord.Embed(description=f"Generating Fakemon...", color=discord.Color.blue())
+ # Generate Unamon description with Poe
+    embed = discord.Embed(description=f"Generating Unamon...", color=discord.Color.blue())
     embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
     msg = await ctx.send(embed=embed)
     try:
-        fakemon_name, fakemon_desc = get_fakemon_info(pClient, words)
+        unamon_name, unamon_desc = get_unamon_info(pClient, words)
     except Exception as e:
         await ctx.send(f"Error: {str(e)}")
         return
     color = discord.Color.random()
-    embed = discord.Embed(title=f"{fakemon_name.upper()}", description=fakemon_desc, color=color)
+    embed = discord.Embed(title=f"{unamon_name.upper()}", description=unamon_desc, color=color)
     embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
     await msg.edit(embed=embed)
     await asyncio.sleep(1)
 
     # Ask if user wants to generate stats from the above description
-    embed = discord.Embed(description=f"Generate stats for **{fakemon_name}**?", color=discord.Color.blue())
+    embed = discord.Embed(description=f"Generate stats for **{unamon_name}**?", color=discord.Color.blue())
     message = await ctx.send(embed=embed)
     await message.add_reaction("👍")
     await message.add_reaction("👎")
@@ -178,15 +178,15 @@ async def gen(ctx, *words):
         
     # If user wants to generate stats, generate them with Poe
     if reaction.emoji == "👍":
-        embed = discord.Embed(description=f"Generating stats for **{fakemon_name}**...", color=discord.Color.blue())
+        embed = discord.Embed(description=f"Generating stats for **{unamon_name}**...", color=discord.Color.blue())
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         msg = await ctx.send(embed=embed)
-        fakemon_stats = generate_stats(pClient, fakemon_name)
-        embed = discord.Embed(title=f"{fakemon_name.upper()} STATS", description=fakemon_stats, color=color)
+        unamon_stats = generate_stats(pClient, unamon_name)
+        embed = discord.Embed(title=f"{unamon_name.upper()} STATS", description=unamon_stats, color=color)
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         await msg.edit(embed=embed)
 
-                # Add the 🎣 reaction to the message
+        # Add the 🎣 reaction to the message
         await msg.add_reaction('🎣')
 
         # Define a function to check if the user reacted with 🎣
@@ -197,29 +197,29 @@ async def gen(ctx, *words):
         try:
             reaction, user = await client.wait_for('reaction_add', timeout=600.0, check=check)
         except asyncio.TimeoutError:
-            embed = discord.Embed(description=f"You didn't catch the {fakemon_name} in time, {ctx.author.mention}!", color=discord.Color.red())
+            embed = discord.Embed(description=f"You didn't catch the {unamon_name} in time, {ctx.author.mention}!", color=discord.Color.red())
             await ctx.send(embed=embed)
         else:
-            # Load the caught fakemon file
-            caught_file = os.path.join(os.path.dirname(__file__), "caught_fakemon.txt")
+            # Load the caught Unamon file
+            caught_file = os.path.join(os.path.dirname(__file__), "caught_unamon.txt")
             if not os.path.exists(caught_file):
                 with open(caught_file, "w") as f:
                     f.write(json.dumps({}))
             with open(caught_file, "r") as f:
                 caught_data = json.loads(f.read())
 
-            # Add the caught fakemon to the file
+            # Add the caught Unamon to the file
             caught_data.setdefault(str(ctx.author.id), [])
             caught_data[str(ctx.author.id)].append({
-                "name": fakemon_name,
-                "description": fakemon_desc,
-                "stats": fakemon_stats,
+                "name": unamon_name,
+                "description": unamon_desc,
+                "stats": unamon_stats,
             })
             with open(caught_file, "w") as f:
                 f.write(json.dumps(caught_data))
 
             # Send a congratulatory message
-            embed = discord.Embed(title=f"Congratulations, {ctx.author.name}!", description=f"You caught **{fakemon_name}**!", color=discord.Color.green())
+            embed = discord.Embed(title=f"Congratulations, {ctx.author.name}!", description=f"You caught **{unamon_name}**!", color=discord.Color.green())
             embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
             await ctx.send(embed=embed)
     elif reaction.emoji == "👎":
@@ -231,39 +231,39 @@ async def gen(ctx, *words):
 async def u(ctx, *, arg=None):
     user_id = str(ctx.author.id)
     
-    with open('caught_fakemon.txt', 'r') as f:
-        caught_fakemon = json.load(f)
+    with open('caught_unamon.txt', 'r') as f:
+        caught_unamon = json.load(f)
     
-    if user_id not in caught_fakemon:
-        embed = discord.Embed(title="Oops!", description="You haven't caught any Fakemon yet!", color=discord.Color.red())
+    if user_id not in caught_unamon:
+        embed = discord.Embed(title="Oops!", description="You haven't caught any Unamon yet!", color=discord.Color.red())
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         await ctx.send(embed=embed)
         return
     
-    fakemon_list = caught_fakemon[user_id]
+    unamon_list = caught_unamon[user_id]
     
     if arg is None:
-        if not fakemon_list:
-            embed = discord.Embed(title="Oops!", description="You haven't caught any Fakemon yet!", color=discord.Color.red())
+        if not unamon_list:
+            embed = discord.Embed(title="Oops!", description="You haven't caught any Unamon yet!", color=discord.Color.red())
             embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
             await ctx.send(embed=embed)
             return
         
-        fakemon_names = [fakemon["name"] for fakemon in fakemon_list]
-        embed = discord.Embed(title=f"{ctx.author.name}'s Caught Fakemon", description="\n".join(fakemon_names), color=discord.Color.blue())
+        unamon_names = [unamon["name"] for unamon in unamon_list]
+        embed = discord.Embed(title=f"{ctx.author.name}'s Caught Unamon", description="\n".join(unamon_names), color=discord.Color.blue())
     else:
-        fakemon = next((f for f in fakemon_list if f["name"].lower() == arg.lower()), None)
+        unamon = next((f for f in unamon_list if f["name"].lower() == arg.lower()), None)
         
-        if fakemon is None:
-            embed = discord.Embed(title="Oops!", description=f"You haven't caught a Fakemon named {arg} yet!", color=discord.Color.red())
+        if unamon is None:
+            embed = discord.Embed(title="Oops!", description=f"You haven't caught an Unamon named {arg} yet!", color=discord.Color.red())
             embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
             await ctx.send(embed=embed)
             return
         
         color = discord.Color.random()
-        embed1 = discord.Embed(title=f"{fakemon['name']}'s Description", description=fakemon["description"], color=color)
+        embed1 = discord.Embed(title=f"{unamon['name']}'s Description", description=unamon["description"], color=color)
         embed1.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
-        embed2 = discord.Embed(title=f"{fakemon['name']}'s Stats", description=fakemon["stats"], color=color)
+        embed2 = discord.Embed(title=f"{unamon['name']}'s Stats", description=unamon["stats"], color=color)
         embed2.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         message = await ctx.send(embed=embed1)
         await message.add_reaction("⬅️")
@@ -298,51 +298,51 @@ async def u(ctx, *, arg=None):
 
 @client.command()
 async def train(ctx, *, name: str):
-    fakemon = get_fakemon_by_name(ctx.author.id, name.title())
-    if fakemon is None:
-        embed = discord.Embed(description="You don't have a Fakemon with that name.", color=discord.Color.red())
+    unamon = get_unamon_by_name(ctx.author.id, name.title())
+    if unamon is None:
+        embed = discord.Embed(description="You don't have an Unamon with that name.", color=discord.Color.red())
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         await ctx.send(embed=embed)
         return
 
-    training_fakemon = get_training_fakemon_by_user(ctx.author.id)
-    if training_fakemon is not None:
-        embed = discord.Embed(description=f"You are already training {training_fakemon[1]}!", color=discord.Color.orange())
+    training_unamon = get_training_unamon_by_user(ctx.author.id)
+    if training_unamon is not None:
+        embed = discord.Embed(description=f"You are already training {training_unamon[1]}!", color=discord.Color.orange())
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         await ctx.send(embed=embed)
         return
 
-    embed = discord.Embed(description=f"You are now training {fakemon['name']}!", color=discord.Color.green())
+    embed = discord.Embed(description=f"You are now training {unamon['name']}!", color=discord.Color.green())
     embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
     await ctx.send(embed=embed)
 
-    # Add the Fakemon to the fakemon_training.txt file with level 1
-    with open("fakemon_training.txt", "a") as f:
-        f.write(f"{ctx.author.id},{fakemon['name']},1,0\n")
+    # Add the Unamon to the unamon_training.txt file with level 1
+    with open("unamon_training.txt", "a") as f:
+        f.write(f"{ctx.author.id},{unamon['name']},1,0\n")
 
 
 @client.event
 async def on_message(message):
     await client.process_commands(message)
 
-    # Check if the author of the message is training a Fakemon
-    training_fakemon = get_training_fakemon_by_user(message.author.id)
-    if training_fakemon is None:
+    # Check if the author of the message is training an Unamon
+    training_unamon = get_training_unamon_by_user(message.author.id)
+    if training_unamon is None:
         return
 
     # Calculate the XP gained from the message
     xp = random.randint(1, 3)
 
-    # Update the Fakemon's XP and level
-    user_id, fakemon_name, level, current_xp = training_fakemon
+    # Update the Unamon's XP and level
+    user_id, unamon_name, level, current_xp = training_unamon
     required_xp = 10 * level ** 2
     current_xp += xp
     if current_xp >= required_xp:
         level += 1
         current_xp = 0
-        embed = discord.Embed(description=f"Congratulations! {message.author.mention}'s {fakemon_name} has leveled up to level {level}!", color=discord.Color.green())
+        embed = discord.Embed(description=f"Congratulations! {message.author.mention}'s {unamon_name} has leveled up to level {level}!", color=discord.Color.green())
         await message.channel.send(embed=embed)
-    update_training_fakemon(user_id, fakemon_name, level, current_xp)
+    update_training_unamon(user_id, unamon_name, level, current_xp)
 
 
 client.run(TOKEN)
